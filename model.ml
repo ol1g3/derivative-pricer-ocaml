7f088a7de213805c0bd9aedf2ff818f14db8a868
oleh <olegprogir@gmail.com>
2025-10-04 15:25:10 +0200

    moved logic to model, initialized Black-Scholes model, added simple analytics

--- model.ml
+++ model.ml
@@ -1,8 +1,55 @@
 (* open Owl *)
+open Pricer
+
 module type Model = sig 
   val r : float
   val sigma: float
-  
-  val generate_path_plain : float -> int -> float array
-  val generate_path_antithetic : float -> int -> float array
+
+  val path_statistics : float array -> (float * float * float) (* mean, max, min *)
+  val run : int -> int -> float -> float -> unit
 end
+
+module BlackScholes : Model = struct
+  let r = 0.05
+  let sigma = 0.2
+
+  let default_params = {
+    nu = r -. (sigma *. sigma) /. 2.0;
+    rho = 0.0;
+    vega = sigma;
+  }
+
+  let path_statistics path =
+    let mean = Array.fold path ~init:0.0 ~f:(+.) /. Float.of_int (Array.length path) in
+    let max_price = Array.fold path ~init:Float.neg_infinity ~f:Float.max in
+    let min_price = Array.fold path ~init:Float.infinity ~f:Float.min in
+    (mean, max_price, min_price)
+
+  let run n_simulations n_steps initial_price initial_volatility =
+    Random.init (Int.of_float (Unix.time ()));
+    let initial_price = Float.log initial_price in
+    let results = Array.init n_simulations ~f:(fun _ -> calculate_price default_params initial_price initial_volatility (generate_normals n_steps)) in
+    let results_antithetic = Array.init n_simulations ~f:(fun _ -> simulate_with_antithetic default_params initial_price initial_volatility n_steps) in
+    
+    let average = Array.fold results ~init:0.0 ~f:(+.) /. Float.of_int n_simulations in
+    let variance = Array.fold results ~init:0.0 ~f:(fun acc x -> acc +. (x -. average) *. (x -. average)) /. Float.of_int n_simulations in
+    Stdio.printf "Plain Monte Carlo:\n";
+    Stdio.printf "Average Result: %f\n" average;
+    Stdio.printf "Variance: %f\n" variance;
+
+    let average_antithetic = Array.fold results_antithetic ~init:0.0 ~f:(+.) /. Float.of_int n_simulations in
+    let variance_antithetic = Array.fold results_antithetic ~init:0.0 ~f:(fun acc x -> acc +. (x -. average_antithetic) *. (x -. average_antithetic)) /. Float.of_int n_simulations in
+    Stdio.printf "\nAntithetic Variates:\n";
+    Stdio.printf "Average Result: %f\n" average_antithetic;
+    Stdio.printf "Variance: %f\n" variance_antithetic;
+    Stdio.printf "Variance Reduction Factor: %f\n" (variance /. variance_antithetic);
+
+    let (avg, max_val, min_val) = path_statistics results in
+    Stdio.printf "Path statistics: avg: %f, min: %f, max: %f\n" avg min_val max_val;
+    let (avg, max_val, min_val) = path_statistics results_antithetic in
+    Stdio.printf "Path statistics: avg: %f, min: %f, max: %f\n" avg min_val max_val
+end
+
+let () = 
+  let module M = BlackScholes in
+  M.run 1000 100 10.0 0.2
--- pricer.ml
+++ pricer.ml
@@ -7,47 +7,29 @@ module Pricer = struct
     vega : float;
   }
 
-  let default_params = {
-    nu = 0.0001;
-    rho = 0.1;
-    vega = 0.001;
-  }
-
   let standard_normal () : float =
     let u1 = Random.float 1.0 in
     let u2 = Random.float 1.0 in
     Float.sqrt (-2.0 *. Float.log u1) *. Float.cos (2.0 *. Float.pi *. u2)
 
-  let generate_normals n : float list =
-    List.init n ~f:(fun _ -> standard_normal ())
+  let generate_normals n : float array =
+    Array.init n ~f:(fun _ -> standard_normal ())
 
-  let calculate_price (params : params) (initial_price : float) (initial_volatility : float) (normals : float list) : float =
-    let rec loop price volatility = function
-      | [] -> price
-      | normal :: rest ->
+  let calculate_price (params : params) (initial_price : float) (initial_volatility : float) (normals : float array) : float =
+    let rec loop price volatility i =
+      if i >= Array.length normals then price
+      else
+        let normal = normals.(i) in
         let next_volatility = params.rho *. volatility +. params.vega *. normal in
         let next_price = price +. params.nu +. (Float.exp volatility) *. normal in
-        loop next_price next_volatility rest
+        loop next_price next_volatility (i + 1)
     in
-    loop initial_price initial_volatility normals
+    loop initial_price initial_volatility 0
 
   let simulate_with_antithetic (params : params) (initial_price : float) (initial_volatility : float) (n_steps : int) : float =
     let normals = generate_normals n_steps in
-    let antithetic_normals = List.map normals ~f:(Float.neg) in
+    let antithetic_normals = Array.map normals ~f:(Float.neg) in
     let price1 = calculate_price params initial_price initial_volatility normals in
     let price2 = calculate_price params initial_price initial_volatility antithetic_normals in
     (price1 +. price2) /. 2.0
-
-  let run () =
-    let n_simulations = 10000 in
-    let n_steps = 100 in
-    let initial_price = Float.log 10.0 in
-    let initial_volatility = 0.0 in
-    let results = List.init n_simulations ~f:(fun _ -> simulate_with_antithetic default_params initial_price initial_volatility n_steps) in
-    let average = List.fold results ~init:0.0 ~f:(+.) /. Float.of_int n_simulations in
-    Stdio.printf "Average Result: %f\n" average
 end
-
-let () =
-  let module P = Pricer in
-  P.run ()
